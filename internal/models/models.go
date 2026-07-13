77984acec416718b375341feb42039e083711433
Adrià Gomis <gomisroca@gmail.com>
2026-07-13 13:29:22 +0200

    adjust certain models to use pointers

--- internal/models/models.go
+++ internal/models/models.go
@@ -13,14 +13,14 @@ const (
 type Destination struct {
 	Type   DestinationType `json:"type"`
 	URL    string          `json:"url"`
-	Secret string          `json:"secret,omitempty"`
+	Secret *string         `json:"secret,omitempty"`
 }
 
 type DispatchRequest struct {
 	Payload      	map[string]any 	`json:"payload"`
 	Destinations 	[]Destination  	`json:"destinations"`
-	Secret    		string 			`json:"secret,omitempty"`
-	EventType 		string 			`json:"event_type,omitempty"`
+	Secret      	*string        	`json:"secret,omitempty"`
+	EventType   	*string        	`json:"event_type,omitempty"`
 }
 
 type AttemptStatus string
@@ -32,11 +32,11 @@ const (
 )
 
 type DeliveryAttempt struct {
-	AttemptNumber int           `json:"attempt_number"`
-	Timestamp     time.Time     `json:"timestamp"`
-	Status        AttemptStatus `json:"status"`
-	HTTPStatus    *int          `json:"http_status,omitempty"`
-	Error         string        `json:"error,omitempty"`
+	AttemptNumber int           	`json:"attempt_number"`
+	Timestamp     time.Time     	`json:"timestamp"`
+	Status        AttemptStatus 	`json:"status"`
+	HTTPStatus    *int          	`json:"http_status,omitempty"`
+	Error         *string       	`json:"error,omitempty"`
 }
 
 type DestinationResult struct {
@@ -53,7 +53,7 @@ type DispatchResponse struct {
 
 type EventRecord struct {
 	EventID   string              `json:"event_id"`
-	EventType string              `json:"event_type,omitempty"`
+	EventType *string             `json:"event_type,omitempty"`
 	Payload   map[string]any      `json:"payload"`
 	CreatedAt time.Time           `json:"created_at"`
 	Results   []DestinationResult `json:"results"`
